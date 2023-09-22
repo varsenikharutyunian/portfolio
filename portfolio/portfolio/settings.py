9e8f3a506d127f12761e7356a3cc584637a5cdf0
varsenikharutyunyane <varsikharutyunyan72@gmail.com>
2023-09-22 13:14:55 +0400

    added hosts

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e5^6vqds7hkyx)&mlo99oklmny1fj2#l+*9%eo5nt1p-=sce+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://varsikhar.pythonanywhere.com/"]
+ALLOWED_HOSTS = [' varsikhar.pythonanywhere.com ']
 
 
 # Application definition
