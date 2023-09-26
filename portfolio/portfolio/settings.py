0d387fc91374dd44bf57ad78777a2dd515dc33aa
varsenikharutyunyane <varsikharutyunyan72@gmail.com>
2023-09-26 14:15:55 +0400

    Added new

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e5^6vqds7hkyx)&mlo99oklmny1fj2#l+*9%eo5nt1p-=sce+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://varsikhar.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, "/static/")
+# STATIC_ROOT =os.path.join(BASE_DIR, "/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
